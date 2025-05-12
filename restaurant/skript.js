e3b20393f67cb0aa9eea9bf04b76f8e421dd05d4
abdyus390 <abdyus390@edu.linkoping.se>
2025-05-12 20:37:54 +0000

    commited?

--- restaurant/skript.js
+++ restaurant/skript.js
@@ -1,27 +1,28 @@
+console.log("JS laddades på startsidan");
+
 const btnLight = document.querySelector("#lightmode");
 const btnDark = document.querySelector("#darkmode");
-const root = document.documentElement;
 const lightIcon = document.querySelector("#lightlogo");
 const darkIcon = document.querySelector("#darklogo");
 
+// document.body.classList.remove("light-theme");
+// btnLight.style.display = "inline";
+// btnDark.style.display = "none";
+// lightIcon.style.display = "none";
+// darkIcon.style.display = "inline";
+
 btnLight.addEventListener("click", function () {
+  document.body.classList.add("light-theme");
   btnLight.style.display = "none";
   btnDark.style.display = "inline";
-
   lightIcon.style.display = "inline";
   darkIcon.style.display = "none";
-
-  root.style.setProperty('--bg-primary', '#eeeeee');
-  root.style.setProperty('--text-primary', '#222222');
 });
 
 btnDark.addEventListener("click", function () {
+  document.body.classList.remove("light-theme");
   btnDark.style.display = "none";
   btnLight.style.display = "inline";
-
   lightIcon.style.display = "none";
   darkIcon.style.display = "inline";
-
-  root.style.setProperty('--bg-primary', '#333333');
-  root.style.setProperty('--text-primary', '#f5f5dc');
 });
