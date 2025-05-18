56273a47a754a4f9a5c8b9e301008e4d2c3a64a1
abdyus390 <abdyus390@edu.linkoping.se>
2025-05-18 17:54:57 +0000

    18/5

--- Restaurang/skript.js
+++ Restaurang/skript.js
@@ -1,30 +1,35 @@
-// console.log("JS laddades på startsidan");
-
 const lightBtn = document.getElementById("lightmode");
 const darkBtn = document.getElementById("darkmode");
 const body = document.body;
 const lightLogo = document.getElementById("lightlogo");
 const darkLogo = document.getElementById("darklogo");
+const lightMenu = document.getElementById("lightmenu");
+const darkMenu = document.getElementById("darkmenu");
 const bokaBtn = document.getElementById("bokabtn");
-const input = document.getElementsByClassName("input");
-
+const bokning = document.getElementById("bokning");
+const bokat = document.getElementById("bokat");
 
 lightBtn.addEventListener("click", () => {
-  body.classList.add("light-theme");
-  lightBtn.style.display = "none";
-  darkBtn.style.display = "inline";
-  lightLogo.style.display = "inline";
-  darkLogo.style.display = "none";
+    body.classList.add("light-theme");
+    lightBtn.style.display = "none";
+    darkBtn.style.display = "inline";
+    lightLogo.style.display = "inline";
+    darkLogo.style.display = "none";
+    lightMenu.style.display = "inline";
+    darkMenu.style.display = "none";
 });
 
 darkBtn.addEventListener("click", () => {
-  body.classList.remove("light-theme");
-  darkBtn.style.display = "none";
-  lightBtn.style.display = "inline";
-  lightLogo.style.display = "none";
-  darkLogo.style.display = "inline";
+    body.classList.remove("light-theme");
+    darkBtn.style.display = "none";
+    lightBtn.style.display = "inline";
+    lightLogo.style.display = "none";
+    darkLogo.style.display = "inline";
+    lightMenu.style.display = "none";
+    darkMenu.style.display = "inline";
 });
 
 bokaBtn.addEventListener("click", () => {
-  input.value = "";
+    bokning.style.display = "none";
+    bokat.style.display = "inline"
 });
