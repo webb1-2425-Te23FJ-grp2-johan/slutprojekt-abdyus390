d4fb829f3f7fce243bb0d61b13f28f8f2ad9852a
abdyus390 <abdyus390@edu.linkoping.se>
2025-05-25 19:44:18 +0000

    25/5

--- Restaurang/skript.js
+++ Restaurang/skript.js
@@ -1,35 +1,40 @@
-const lightBtn = document.querySelectorAll(".lightmode"); // Hämta alla lightmode-knappar
-const darkBtn = document.querySelectorAll(".darkmode"); // Hämta alla darkmode-knappar
 const body = document.body;
+const lightBtn = document.querySelector(".lightmode");
+const darkBtn = document.querySelector(".darkmode");
 const lightLogo = document.querySelector("#lightlogo");
 const darkLogo = document.querySelector("#darklogo");
-const lightMenu = document.querySelector("#lightmenu");
-const darkMenu = document.querySelector("#darkmenu");
+const burgerMenu = document.getElementById('burger-menu');
+const overlay = document.getElementById('small');
 
-lightBtn.forEach(Btn => {
-    Btn.addEventListener("click", () => { 
-        body.classList.add("light-theme");
-        Btn.style.display = "none"; 
-        darkBtn.forEach(button => {
-            button.style.display = "inline"; 
-        });
-        lightLogo.style.display = "inline";
-        darkLogo.style.display = "none"; 
-        lightMenu.style.display = "inline"; 
-        darkMenu.style.display = "none"; 
-    });
+
+
+lightBtn.addEventListener("click", () => {
+    body.classList.add("light-theme");
+    lightBtn.style.display = "none";
+    darkBtn.style.display = "inline";
+    lightLogo.style.display = "inline";
+    darkLogo.style.display = "none";
 });
 
-darkBtn.forEach(Btn => {
-    Btn.addEventListener("click", () => { 
-        body.classList.remove("light-theme");
-        Btn.style.display = "none";
-        lightBtn.forEach(button => {
-            button.style.display = "inline";
-        });
-        lightLogo.style.display = "none";
-        darkLogo.style.display = "inline";
-        lightMenu.style.display = "none";
-        darkMenu.style.display = "inline";
+darkBtn.addEventListener("click", () => {
+    body.classList.remove("light-theme");
+    darkBtn.style.display = "none";
+    lightBtn.style.display = "inline";
+    lightLogo.style.display = "none";
+    darkLogo.style.display = "inline";
+});
+
+
+
+burgerMenu.addEventListener('click', function() {
+  this.classList.toggle("close"); 
+  overlay.classList.toggle("overlay"); 
+});
+
+
+overlay.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', () => {
+        burgerMenu.classList.remove("close");
+        overlay.classList.remove("overlay");
     });
 });
